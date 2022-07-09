fe0f2d30b818dc19ff6f6aac836a8e80c61b52c8
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-07-09 21:29:53 +0530

    feat : render components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
+import { CreatPost, NavBar } from "./Components ";
+import { Posts } from "./Posts";
 
 function App() {
+  const { register, handleSubmit, reset, setValue } = useForm();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div className="App">
+      <NavBar />
+      <Posts
+        open={open}
+        setOpen={setOpen}
+        handleClose={handleClose}
+        handleOpen={handleOpen}
+        register={register}
+        handleSubmit={handleSubmit}
+        reset={reset}
+        setValue={setValue}
+      />
     </div>
   );
 }
